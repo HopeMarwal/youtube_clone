b52b559e67644c177f32e75b8aa898622f85a9f4
Cascaval Nadejda <vremea.dr@gmail.com>
2023-02-21 11:59:39 +0000

    Initial config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import  { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material'
 //Components
-import Navbar from './components/Navbar';
-import Feed from './components/Feed';
-import VideoDetail from './components/VideoDetail';
-import ChannelDetail from './components/ChannelDetail';
-import SearchFeed from './components/SearchFeed';
+import { Navbar, VideoDetail, ChannelDetail, SearchFeed, Feed } from './components';
 
 const App = () => (
   <div>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material'
 
 export default function Feed() {
   return (
-    <div>Feed</div>
+    <Stack
+    sx={{ flexDirection: {sx: 'column', md: 'row'} }}
+    >
+      <Box
+
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2}
+        }}
+      >
+
+      </Box>
+    </Stack>
   )
 }
