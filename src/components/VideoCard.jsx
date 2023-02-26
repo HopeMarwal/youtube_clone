6978ccb312ff56b0dd387fe46f770165752ebca7
Cascaval Nadejda <vremea.dr@gmail.com>
2023-02-26 11:16:23 +0000

    Video card Img

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,8 +3,21 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video: {id : { videoId }, snippet } }) {
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={ videoId? `/video/${videoId}` : demoVideoUrl }>
+        <CardMedia
+          image={ snippet?.thumbnails?.high?.url }
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent
+        sx={{ backgroundColor: '#1e1e1e', height: '106px' }}
+      >
+        {/* ToDo */}
+      </CardContent>
+    </Card>
   )
 }
