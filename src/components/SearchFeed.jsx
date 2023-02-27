48d97f3189a82aab7a29b0bd48b62790ccdddd50
Cascaval Nadejda <vremea.dr@gmail.com>
2023-02-27 17:55:02 +0000

    Search Bar component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+//React
+import { useState, useEffect } from 'react';
+//UI material
+import { Box, Typography } from '@mui/material'
+//Router
+import { useParams } from 'react-router-dom'
+//Components
+import { Videos } from './'
+//API
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 export default function SearchFeed() {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => {
+        setVideos(data.items)
+      })
+  }, [searchTerm])
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+
+      {/* Heading */}
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+
+        Search results for: <span style={{ color: '#f31503' }}>{searchTerm}</span> videos
+
+      </Typography>
+
+      {/* Videos */}
+      <Videos videos={videos} />
+    </Box>
   )
 }
