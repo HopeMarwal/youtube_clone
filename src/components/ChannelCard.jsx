c14137bcc8e777aef2a701cb79372988e87e0b5f
Cascaval Nadejda <vremea.dr@gmail.com>
2023-02-27 14:02:16 +0000

    ChannelCard component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography} from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
-export default function ChannelCard() {
+export default function ChannelCard({ channelDetail }) {
+
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { md: '368px', xs: '356px'},
+        height: '326px',
+        margin: 'auto'
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff'
+          }}
+        >
+          {/* Channel logo */}
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: '50%',
+              height: 180,
+              width: 180,
+              mb: 2,
+              border: '1px solid #e3e3e3'
+            }}
+          />
+          {/* Channel title */}
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
+          </Typography>
+          {/* SubscriberCount */}
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -15,7 +15,7 @@ export default function Videos({ videos }) {
             key={index}
           >
             {item.id.videoId && <VideoCard video={item} />}
-            {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
           </Box>
         ))
       }
